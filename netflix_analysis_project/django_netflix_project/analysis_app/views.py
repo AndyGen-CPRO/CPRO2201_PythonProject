c393f68bf24977da5a18b4ee02dacc99792d19e4
AndyGen-CPRO <christianandy.generoso.dl@gmail.com>
2025-04-07 14:36:12 -0600

    - Added genre filtering ( all genre present in "listed_in" column are split by commas ) - Created a range for year filtering based on the first and last number in the dataset - Split the countries present in the dataset by commas to separate all of them

--- netflix_analysis_project/django_netflix_project/analysis_app/views.py
+++ netflix_analysis_project/django_netflix_project/analysis_app/views.py
@@ -5,13 +5,14 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Media
 from .forms import MediaForm
-from django.db.models import Q
+from django.db.models import Q, Min, Max
 
 def media_list(request):
     query = request.GET.get('q', '')
     type = request.GET.get('type', '')
     year = request.GET.get('year', '')
     country = request.GET.get('country', '')
+    genre = request.GET.get('genre', '')
 
     medias = Media.objects.all()
 
@@ -29,13 +30,45 @@ def media_list(request):
         medias = medias.filter(release_year=year)
 
     if country:
-        medias = medias.filter(country=country)
+        medias = medias.filter(country__icontains=country)
+    
+    if genre:
+        medias = medias.filter(listed_in__icontains=genre)
+
+    #To get the range of all the years present in the dataset
+    min_year = Media.objects.aggregate(Min('release_year'))['release_year__min'] or 1900
+    max_year = Media.objects.aggregate(Max('release_year'))['release_year__max'] or 2025
+    all_years = list(range(min_year, max_year + 1))
+
+    #Filtering the countries present in the dataset that was separated by commas
+    raw_countries = Media.objects.values_list('country', flat=True)
+
+    country_set = set()
+    for entry in raw_countries:
+        if entry:
+            country_list = [c.strip() for c in entry.split(',') if c.strip()]
+            country_set.update(country_list)
+
+    all_countries = sorted(country_set)
+    all_countries.insert(0, 'Empty')
+
+    #Filtering the genre/catagories present in the dataset
+    raw_genres = Media.objects.values_list('listed_in', flat=True)
+
+    genre_set = set()
+    for entry in raw_genres:
+        if entry:
+            genre_list = [g.strip() for g in entry.split(',') if g.strip()]
+            genre_set.update(genre_list)
+    
+    all_genres = sorted(genre_set)
 
     context = {
         'medias': medias,
         'types': Media.objects.values_list('type', flat=True).distinct(),
-        'years': Media.objects.values_list('release_year', flat=True).distinct(),
-        'countries': Media.objects.values_list('country', flat=True).distinct()
+        'years': all_years,
+        'countries': all_countries,
+        'genres': all_genres
     }
     return render(request, 'analysis_app/media_list.html', context)
 
